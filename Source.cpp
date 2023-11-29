fa919f33db6d3a2b37f171e37af0e87e108f39fb
evident0 <61017717+evident0@users.noreply.github.com>
2023-11-29 22:14:03 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -77,9 +77,7 @@ void inputKeys() {
 
 }
 
-//************************************
-// Η LoadShaders είναι black box για σας
-//************************************
+
 GLuint LoadShaders(const char* vertex_file_path, const char* fragment_file_path) {
 
 	// Create the shaders
